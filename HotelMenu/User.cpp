705906d3b538a1e90cfb831a92cc64ca14f52603
mcmacska <42695936+mcmacska@users.noreply.github.com>
2020-11-17 19:47:33 +0100

    user

--- HotelMenu/User.cpp
+++ HotelMenu/User.cpp
@@ -1,7 +1,13 @@
 #include "user.h"
-std::string Users::User::getPassword() const
+
+std::string Users::User::getFirstName() const
 {
-    return password;
+    return firstName;
+}
+
+std::string Users::User::getLastName() const
+{
+    return lastName;
 }
 
 std::string Users::User::getUsername() const
@@ -9,9 +15,24 @@ std::string Users::User::getUsername() const
     return username;
 }
 
-Users::User::~User()
+date Users::User::getBirthDate() const
 {
+    return birthDate;
+}
 
+Users::jobs Users::User::getJobID() const
+{
+    return jobID;
+}
+
+int Users::User::getCardID() const
+{
+    return cardID;
+}
+
+std::string Users::User::getPassword() const
+{
+    return password;
 }
 
 Users::User::User()
@@ -19,9 +40,17 @@ Users::User::User()
 
 }
 
-Users::User::User(const std::string& _username, const std::string& _password,jobs _jobID)
+Users::User::User(const std::string &_firstName, const std::string &_lastName, const std::string &_username, struct date _birthDate, Users::jobs _jobID, int _cardID, const std::string &_password):
+    firstName(_firstName),
+    lastName(_lastName),
+    username(_username),
+    birthDate(_birthDate),
+    jobID(_jobID),
+    cardID(_cardID),
+    password(_password)
 {
-    username=_username;
-    password=_password;
-    jobID=_jobID;
+}
+
+Users::User::~User()
+{
 }
--- HotelMenu/User.h
+++ HotelMenu/User.h
@@ -2,6 +2,9 @@
 #define RIGHTSANDCOMMANDS_H
 #include "map"
 #include "string"
+#include "date.h"
+#include "taskcontainer.h"
+#include "log.h"
 
 namespace  Users
 {
@@ -14,18 +17,33 @@ namespace  Users
     };
     class User
     {
-
+    protected:
+        std::string firstName;
+        std::string lastName;
         std::string username;
-        std::string password;
+        struct date birthDate;
         jobs jobID;
+        int cardID;
+        std::string password;
+        TaskContainer taskCont;
     public:
         User();
-        User(const std::string& _username, const std::string& _password,jobs _jobID);
-        std::string getPassword() const;
+        User(const std::string& _firstName , const std::string& _lastName , const std::string& _username , struct date _birthDate , jobs _jobID , int _cardID , const std::string& _password);
+
+        std::string getFirstName() const;
+        std::string getLastName() const;
         std::string getUsername() const;
+        date getBirthDate() const;
+        jobs getJobID() const;
+        int getCardID() const;
+        std::string getPassword() const;
+
+        void setLog(const std::string& task_id , Logs::Log* onelog);
+
         virtual void doWork()const=0;
         virtual User * clone()const=0;
         virtual ~User();
+
 
     };
 } // eof Users
