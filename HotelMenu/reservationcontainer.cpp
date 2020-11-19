b602bdce204c688bfd9a1ec1af9cdb40ede3a4eb
AdamBeer2000 <57035872+AdamBeer2000@users.noreply.github.com>
2020-11-19 22:17:28 +0100

    reservation cont

--- HotelMenu/reservationcontainer.cpp
+++ HotelMenu/reservationcontainer.cpp
@@ -4,3 +4,26 @@ ReservationContainer::ReservationContainer()
 {
 
 }
+
+void ReservationContainer::addReservation(Reservation newRes)
+{
+    reservations.insert(std::pair<std::string,Reservation>(newRes.getUserename(),newRes));
+}
+
+void ReservationContainer::deleteReservation(std::string guestName)
+{
+    if(reservations.find(guestName)!=reservations.end())
+    {
+        reservations.erase(reservations.find(guestName));
+    }
+}
+
+void ReservationContainer::bookRoom(std::string _userename, Suit::suitTypes _apartment, date _startTime, date _endTime, Serving::servingTypes _serving)
+{
+    reservations.insert(std::pair<std::string,Reservation>(_userename,Reservation(_userename,_apartment,_startTime,_endTime,_serving)));
+}
+
+Reservation ReservationContainer::getReservation(std::string guestName)
+{
+    return reservations.find(guestName)->second;
+}
--- HotelMenu/reservationcontainer.h
+++ HotelMenu/reservationcontainer.h
@@ -1,11 +1,21 @@
 #ifndef RESERVATIONCONTAINER_H
 #define RESERVATIONCONTAINER_H
-
+#include "map"
+#include "reservation.h"
 
 class ReservationContainer
 {
+    std::map<std::string,Reservation>reservations;
 public:
     ReservationContainer();
+    void addReservation(Reservation newRes);
+    void deleteReservation(std::string guestName);
+    void bookRoom(std::string _userename,Suit::suitTypes _apartment,date _startTime,date _endTime,Serving::servingTypes _serving);
+
+    Reservation getReservation(std::string guestName);
+
+
+
 };
 
 #endif // RESERVATIONCONTAINER_H
