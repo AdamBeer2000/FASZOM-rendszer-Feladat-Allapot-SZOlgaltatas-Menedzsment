d7090cb0bf1192a5725c83545ebba77bbbe00f0d
AdamBeer2000 <57035872+AdamBeer2000@users.noreply.github.com>
2020-12-13 00:37:24 +0100

    Revert "Merge branch 'master' of https://github.com/AdamBeer2000/FASZOM-rendszer-Feladat-Allapot-SZOlgaltatas-Menedzsment"
    This reverts commit 6abf3bd8ea10c27a997e06e7c270191842a8af85, reversing
    changes made to a9a3e813d3bc642be496a4a82e3af08db4591198.

--- HotelMenu/commandpanel.cpp
+++ HotelMenu/commandpanel.cpp
@@ -209,6 +209,8 @@ void CommandPanel::replace()
 }
 void CommandPanel::takeCleanroom()
 {
+    std::string task_id;
+    bool invalid = false;
     std::cout << "-------------------------------------------------------------------" << std::endl;
     int roomId;
     std::cout<<"Melyik szobat takaritanad ki? " << std::flush;
